e474817989303dcfd3d9e408eb26166bec73edc1
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-22 17:21:13 +1000

    Add swaps counter

--- bubbleSort.js
+++ bubbleSort.js
@@ -42,26 +42,31 @@ const bubbleSort = (array) => {
   let swapped = true; // Assume the worst
   // We don't need to consider elements that have bubbled to the end.
   let end = array.length;
+  let swaps = 0
 
   while (swapped === true) {
     swapped = false;
     for (let i = 0; i < end; i++) {
-      if (array[i] > array[i + 1]) {
 
-        // ES6ier with parallel assignment/destructuring:
-        // [ array[i], array[i+1] ] = [ array[i+1], array[i] ];
+      if (array[i] > array[i + 1]) {
 
         let temp = array[i];
         array[i] = array[i+1];
         array[i+1] = temp;
+
+        // ES6ier with parallel assignment/destructuring:
+        // [ array[i], array[i+1] ] = [ array[i+1], array[i] ];
+
+        swaps = swaps + 1
 
         swapped = true;
       }
     }
     end--;
   }
+  console.log(`Array is sorted in ${swaps} swaps`)
 
-  return array;
+  return swaps;
 }
 
 console.log(bubbleSort([6,4,1]));
